df10d96dd2149b86b3fd3159cddd8c0359241039
Juan Carlos Paz H <juan.paz@idu.gov.co>
2017-03-29 12:17:50 -0500

    se agrega en el filtro otro header para el crossdomain

--- src/java/co/idu/ws/rest/CORSResponseFilter.java
+++ src/java/co/idu/ws/rest/CORSResponseFilter.java
@@ -23,7 +23,7 @@ public class CORSResponseFilter implements ContainerResponseFilter{
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
         headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        headers.add("Access-Control-Allow-Credentials", "true");
         headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, accept, authorization, origin");
+        headers.add("Access-Control-Expose-Headers", "X-Requested-With, Content-Type, accept, authorization, origin");
     }
 }
